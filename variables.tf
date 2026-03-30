a4dacdf0e97430ac8aef91a5e6e3ab852366e432
aboubacar <sidikinabe10@gmail.com>
2026-03-30 14:25:07 +0200

    Add validation rules for virtual machine configuration in Terraform (exo 7)

--- variables.tf
+++ variables.tf
@@ -27,3 +27,47 @@ variable "server_names" {
   type        = list(string)
   default     = ["alpha", "beta", "gamma"]
 }
+
+variable "machines" {
+  description = "Liste des machines virtuelles à déployer"
+  type = list(object({
+    name      = string
+    vcpu      = number
+    disk_size = number
+    region    = string
+  }))
+
+  default = [
+    {
+      name      = "vm-app-1"
+      vcpu      = 2
+      disk_size = 20
+      region    = "eu-west-1"
+    }
+  ]
+
+  validation {
+    condition = alltrue([
+      for machine in var.machines : machine.vcpu >= 2 && machine.vcpu <= 64
+    ])
+    error_message = "Chaque machine doit avoir un vcpu entre 2 et 64."
+  }
+
+  validation {
+    condition = alltrue([
+      for machine in var.machines : machine.disk_size >= 20
+    ])
+    error_message = "Chaque machine doit avoir un disk_size supérieur ou égal à 20 Go."
+  }
+
+  validation {
+    condition = alltrue([
+      for machine in var.machines : contains([
+        "eu-west-1",
+        "us-east-1",
+        "ap-southeast-1"
+      ], machine.region)
+    ])
+    error_message = "Chaque machine doit avoir une region parmi: eu-west-1, us-east-1, ap-southeast-1."
+  }
+}
